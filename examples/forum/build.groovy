1c5b79dd0bd648883a6823866d5253f186cf79cd
Lyndon <l.leong1618@gmail.com>
2023-02-18 09:15:11 +0000

    add forum example

---
+++ examples/forum/build.groovy
@@ -0,0 +1,22 @@
+imports: Imports {
+    std,
+    build,
+}
+
+build:  build.Builder {
+    body:  {
+        // target platform options
+        target: self.Target.standardTargetOptions()
+        // relase, debug ...
+        mode: self.Mode.standardModeOptions()
+        // 
+        exe: self.Exe.standardExeOptions()
+
+        runCmd: exe.run()
+        If (self.args){
+            body:  {
+                runCmd.addArgs(self.args)
+            }
+        }
+    }
+}
