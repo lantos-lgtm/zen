32f2cf30f5aeb9675e80fa264a1b18fef52625d7
Lyndon <l.leong1618@gmail.com>
2023-02-19 13:53:01 +0000

    add maps

--- std/data/map.groovy
+++ std/data/map.groovy
@@ -1,4 +1,14 @@
-
+std: std()
+{
+    Array,
+    Boolean,
+    Function,
+    HashFunc,
+    Int,
+    String,
+    Type,
+    Void,
+}: std.types
 
 Map: Type {
     keys: Array {
@@ -6,11 +16,29 @@ Map: Type {
     },
     values: Array {
         self: {Type}
-    }
-    ordered: Bool
+    },
+    ordered: Bool,
+    hashFunc: HashFunc
 }
 
-
+Map: Function {
+    args: {
+        self: Type
+    }
+    return: Map {
+        keys: Array {String},
+        values: Array {Type},
+        ordered: Bool,
+        hashFunc: HashFunc
+    }
+    body: {
+        fieldPairsLoop: Loop(self.fieldPairs) {
+            return.keys.push(fieldPairsLoop.value.key)
+            return.values.push(fieldPairsLoop.value.value)
+        }
+        return()
+    }
+}
 
 // insert
 
